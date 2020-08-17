523ce35713a14f7bdf23604377ebf7329128ab1a
sudo.park <sudo.park@kakaocorp.com>
2020-08-17 13:53:43 +0900

    add demand base publishing to custom publisher and subscriptions

--- Sources/Combine+Extensions/Socket+Combine+Extensions.swift
+++ Sources/Combine+Extensions/Socket+Combine+Extensions.swift
@@ -16,38 +16,83 @@ public enum SocketStatusEvent {
     case onOpen
     case onClose
     case onError(_ error: Error)
+        
+    
+    enum EventType {
+        case open
+        case close
+        case error
+        case all
+    }
+}
+
+@available(iOS 13.0, *)
+protocol DemandBasePublishing: class {
+    var publishedElementCount: Int { get set }
+    var currentDemand: Subscribers.Demand! { get set }
+}
+
+@available(iOS 13.0, *)
+extension DemandBasePublishing {
+    
+    private func shouldPublishElement() -> Bool {
+        guard let currentDemand = self.currentDemand else { return false }
+        guard let max = currentDemand.max else {
+            return true
+        }
+        let remainCount = max - self.publishedElementCount
+        return remainCount > 0
+    }
+    
+    func publishElementOrNot<S: Subscriber>(_ subscriber: S, element: S.Input) {
+        guard self.shouldPublishElement() else { return }
+        let additionalDemand = subscriber.receive(element)
+        self.publishedElementCount += 1
+        self.currentDemand += additionalDemand
+    }
 }
 
 
 // MARK: - SocketEventSubecription
 
 @available(iOS 13.0, *)
-class SocketEventSubscription<O, S: Subscriber>: Subscription where S.Input == O, S.Failure == Never {
+class SocketEventSubscription<O, S: Subscriber>: Subscription, DemandBasePublishing where S.Input == O, S.Failure == Never {
     
     typealias Listening = (Socket, @escaping (O) -> Void) -> Void
     
     private weak var socket: Socket?
     private var subscriber: S?
     private let listening: Listening
+    var publishedElementCount: Int = 0
+    var currentDemand: Subscribers.Demand!
+    private let lock = NSRecursiveLock()
     
     init(socket: Socket, listening: @escaping Listening, subscriber: S) {
         self.socket = socket
         self.listening = listening
         self.subscriber = subscriber
-    }
-    
-    func request(_ demand: Subscribers.Demand) {
+        
         self.listenSocketEvents()
     }
     
+    func request(_ demand: Subscribers.Demand) {
+        self.lock.lock()
+        self.publishedElementCount = 0
+        self.currentDemand = demand
+        self.lock.unlock()
+    }
+    
     func cancel() {
-        subscriber = nil
+        self.lock.lock()
+        self.subscriber = nil
+        self.lock.unlock()
     }
     
     private func listenSocketEvents() {
-        guard let subscriber = self.subscriber, let socket = self.socket else { return }
-        self.listening(socket) { output in
-            _ = subscriber.receive(output)
+        guard let socket = self.socket else { return }
+        self.listening(socket) { [weak self] output in
+            guard let self = self, let subscriber = self.subscriber else { return }
+            self.publishElementOrNot(subscriber, element: output)
         }
     }
 }
@@ -61,22 +106,31 @@ internal struct SocketStatusEventPublisher: Publisher {
     typealias Failure = Never
     
     private let socket: Socket
+    private let eventType: SocketStatusEvent.EventType
     
-    internal init(socket: Socket) {
+    internal init(socket: Socket, eventType: SocketStatusEvent.EventType) {
         self.socket = socket
+        self.eventType = eventType
     }
     
     internal func receive<S>(subscriber: S) where S : Subscriber, Self.Failure == S.Failure, Self.Output == S.Input {
         
+        let type = self.eventType
         let listenStatusEvents: (Socket, @escaping (SocketStatusEvent) -> Void) -> Void = { socket, callback in
-            socket.onOpen {
-                callback(.onOpen)
+            if type == .open || type == .all {
+                socket.onOpen {
+                    callback(.onOpen)
+                }
             }
-            socket.onClose {
-                callback(.onClose)
+            if type == .close || type == .all {
+                socket.onClose {
+                    callback(.onClose)
+                }
             }
-            socket.onError { error in
-                callback(.onError(error))
+            if type == .error || type == .all {
+                socket.onError { error in
+                    callback(.onError(error))
+                }
             }
         }
         let subscription = SocketEventSubscription(socket: socket, listening: listenStatusEvents, subscriber: subscriber)
@@ -137,40 +191,34 @@ extension Socket {
 @available(iOS 13.0, *)
 extension SocketPublishers {
     
-    internal var _socketEvents: SocketStatusEventPublisher {
-        return SocketStatusEventPublisher(socket: self.socket)
+    private func socketEventPublsidher(for type: SocketStatusEvent.EventType) -> SocketStatusEventPublisher {
+        return SocketStatusEventPublisher(socket: self.socket, eventType: type)
     }
     
     public var statusEvents: AnyPublisher<SocketStatusEvent, Never> {
-        return self._socketEvents
+        return self.socketEventPublsidher(for: .all)
             .eraseToAnyPublisher()
     }
     
     public var onOpen: AnyPublisher<Void, Never> {
-        return statusEvents
-            .compactMap { event -> Void? in
-                guard case .onOpen = event else { return nil }
-                return ()
-        }
-        .eraseToAnyPublisher()
+        return self.socketEventPublsidher(for: .open)
+            .map{ _ in }
+            .eraseToAnyPublisher()
     }
     
     public var onClose: AnyPublisher<Void, Never> {
-        return statusEvents
-            .compactMap { event -> Void? in
-                guard case .onClose = event else { return nil }
-                return ()
-        }
-        .eraseToAnyPublisher()
+        return self.socketEventPublsidher(for: .close)
+            .map{ _ in }
+            .eraseToAnyPublisher()
     }
     
     public var onError: AnyPublisher<Error, Never> {
-        return statusEvents
-            .compactMap { event -> Error? in
-                guard case let .onError(error) = event else { return nil }
+        return self.socketEventPublsidher(for: .error)
+            .compactMap { status -> Error? in
+                guard case let .onError(error) = status else { return nil }
                 return error
-        }
-        .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
     }
     
     public var onMessage: AnyPublisher<Message, Never> {
--- Tests/Combine+Extensions/Channel+Combine+Extensions+Tests.swift
+++ Tests/Combine+Extensions/Channel+Combine+Extensions+Tests.swift
@@ -53,107 +53,107 @@ extension ChannelCombineExtensionsTests {
     func testChannel_joinSuccess() {
         // given
         let expect = expectation(description: "join the channel")
-        
+
         // when
         let result = self.waitPushResult(expect, source: self.channel.join().eraseToAnyPublisher()) {
             self.channel.joinPush?.trigger("ok", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isSuccess, true)
     }
-    
+
     func testChannel_notLeak() {
         // given
         weak var channel: Channel? = Channel(topic: "test", socket: self.mockSocket)
-        
+
         // when
         channel?.publishers.on(event: "some")
             .sink(receiveValue: { _ in })
             .store(in: &self.cancellables)
-        
+
         // then
         XCTAssertNil(channel)
     }
-    
+
     func testChannel_joinError() {
         // given
         let expect = expectation(description: "join channel error")
-        
+
         // when
         let result = self.waitPushResult(expect, source: self.channel.join().eraseToAnyPublisher()) {
             self.channel.joinPush?.trigger("error", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isFail, true)
     }
-    
+
     func testChannel_joinTimeoutError() {
         // given
         let expect = expectation(description: "join channel timeout error")
-        
+
         // when
         let result = self.waitPushResult(expect, source: self.channel.join().eraseToAnyPublisher()) {
             self.channel.joinPush?.trigger("timeout", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isTimeoutFail, true)
     }
-    
+
     func testChannel_leaveSuccess() {
         // given
         let expect = expectation(description: "leave channel success")
-        
+
         // when
         let result = self.waitPushResult(expect, source: self.channel.leave().eraseToAnyPublisher()) { }
-        
+
         // then
         XCTAssertEqual(result?.isSuccess, true)
     }
-    
+
     func testChannel_sendPush() {
         // given
         let expect = expectation(description: "send push to channel")
         self.channel.joinedOnce = true
-        
+
         // when
         let push = self.channel.push("test", payload: [:])
         let result = self.waitPushResult(expect, source: push.eraseToAnyPublisher()) {
             push.trigger("ok", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isSuccess, true)
     }
-    
+
     func testChannel_sendPushFail() {
         // given
         let expect = expectation(description: "send push to channel fail")
         self.channel.joinedOnce = true
-        
+
         // when
         let push = self.channel.push("test", payload: [:])
         let result = self.waitPushResult(expect, source: push.eraseToAnyPublisher()) {
             push.trigger("error", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isFail, true)
     }
-    
+
     func testChannel_sendPushTimeout() {
         // given
         let expect = expectation(description: "send push to channel timeout")
         self.channel.joinedOnce = true
-        
+
         // when
         let push = self.channel.push("test", payload: [:])
         let result = self.waitPushResult(expect, source: push.eraseToAnyPublisher()) {
             push.trigger("timeout", payload: [:])
         }
-        
+
         // then
         XCTAssertEqual(result?.isTimeoutFail, true)
     }
@@ -168,42 +168,61 @@ extension ChannelCombineExtensionsTests {
     func testChannel_onClose() {
         // given
         let expect = expectation(description: "channel subscribe close event")
-        
+
         // when
         let closeCount = self.wait(expect, source: self.channel.publishers.onClose) {
             self.channel.leave()
         }.count
-        
+
         // then
         XCTAssertEqual(closeCount, 1)
     }
-    
+
     func testChannel_onError() {
         // given
         let expect = expectation(description: "channel subscribe error event")
-        
+
         // when
         let errorCount = self.wait(expect, source: self.channel.publishers.onError) {
             self.channel.trigger(event: ChannelEvent.error)
         }.count
-        
+
         // then
         XCTAssertEqual(errorCount, 1)
     }
-    
+
     func testChannel_onCustomEvents() {
         // given
         let expect = expectation(description: "channel subscribe custom events")
-        
+
         // when
         let events = self.wait(expect, source: self.channel.publishers.on(event: "event1")) {
             self.channel.trigger(event: "event")
             self.channel.trigger(event: "event1")
             self.channel.trigger(event: "event2")
         }
-        
+
         // then
         XCTAssertEqual(events.count, 1)
+    }
+    
+    func testChannel_emitEventDependOnSubscriberDemand() {
+        // given
+        let spySubscriber = SpyChannelEventSubscriber()
+        self.channel.publishers.on(event: "event")
+            .subscribe(spySubscriber)
+        
+        // when
+        func publishEvent(_ count: Int) {
+            let payload: [String: Any] = ["count": count]
+            self.channel.trigger(event: "event", payload: payload)
+        }
+        (0..<100).forEach {
+            publishEvent($0)
+        }
+        
+        // then
+        XCTAssertEqual(spySubscriber.recordedCounts, Array(0..<10))
     }
 }
 
@@ -231,5 +250,38 @@ private extension Result where Success == Message, Failure == Error {
                 return false
         }
         return true
+    }
+}
+
+@available(iOS 13.0, *)
+extension ChannelCombineExtensionsTests {
+    
+    class SpyChannelEventSubscriber: Subscriber {
+        
+        typealias Input = Message
+        typealias Failure = Never
+        
+        var recordedCounts = [Int]()
+        private var subscription: Subscription?
+        
+        func receive(subscription: Subscription) {
+            self.subscription = subscription
+            subscription.request(.max(7))
+        }
+        
+        func receive(_ input: Message) -> Subscribers.Demand {
+            let payload = input.payload
+            let count = payload["count"] as? Int ?? 0
+            self.recordedCounts.append(count)
+            if count == 6 {
+                return .max(3)
+            } else {
+                return .none
+            }
+        }
+        
+        func receive(completion: Subscribers.Completion<Never>) {
+            subscription = nil
+        }
     }
 }
